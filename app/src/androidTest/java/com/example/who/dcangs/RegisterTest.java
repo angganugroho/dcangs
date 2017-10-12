3efe71d1c315e1e0d7bd96fcfa6acb40e0ce5059
DESKTOP-7I631A8\Daud <daud.muhajir11@gmail.com>
2017-10-12 16:10:44 +0700

    tambah register test

---
+++ app/src/androidTest/java/com/example/who/dcangs/RegisterTest.java
@@ -0,0 +1,22 @@
+package com.example.who.dcangs;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Daud on 12-Oct-17.
+ */
+public class RegisterTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+}
